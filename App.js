d3d2599b4ecf5a60da3dcd9ad8a2d2c186368f80
unknown <u070267@dur-sshett2-2w7.tsh.thomson.com>
2015-06-18 15:34:30 -0400

    Ran Build

--- App.js
+++ App.js
@@ -43,42 +43,4 @@ Ext.define('CustomApp', {
 
 
     }
-
-    /*
-    launch: function() {
-        //Write app code here
-        console.log('My first Rally App for Features Sudhir WOOT!!!');
-        var myUserStoryStore=Ext.create('Rally.data.wsapi.Store', {
-            model: 'PortfolioItem/Feature',
-            autoLoad: true,
-            listeners: {
-                load: function(myUStoryStore, myStorydata, success) {
-                    //process data
-                    console.log('!!Got User Feature Data',myUStoryStore,myStorydata,success);
-                    // Add grid
-                    var myUserStoryGrid=Ext.create('Ext.Container', {
-                        items: [{
-                            xtype: 'rallygrid',
-                            columnCfgs: [
-                                'FormattedID',
-                                'Name','LastUpdateDate'
-                            ],
-                            storeConfig: {
-                                model: 'PortfolioItem/Feature'
-                            }
-                        }],
-                        renderTo: Ext.getBody()
-                    });
-                    //Add Grid ends
-                    console.log('My Feature grid',myUserStoryGrid);
-                    this.add(myUserStoryGrid);
-                },
-                scope:this
-            },
-            fetch: ['FormattedID','Name', 'LastUpdateDate']
-        });
-        //API Docs: https://help.rallydev.com/apps/2.0/doc/
-    }
-    */
-
 });
